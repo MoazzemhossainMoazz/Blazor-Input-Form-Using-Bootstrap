2cf2e90e30b696497468760a517b826c18ead2ea
Md Moazz <moazzemh222@gmail.com>
2025-01-30 00:46:12 +0600

    ERBIS Customer Info

--- BlazorInputForm/BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorInputForm/BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb8d7f2c2338a9b587f8e8707729526aba09019")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05771116fbbad58c49144103237d168e082b33d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
