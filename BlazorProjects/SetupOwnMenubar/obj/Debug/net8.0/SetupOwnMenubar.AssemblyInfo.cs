88bc005fd717455c5feceb42e48a415bf03c858e
unknown <moazzemh222@gmail.com>
2025-02-17 18:30:58 +0600

    BlazorWebAppUsingRadzen

--- BlazorProjects/SetupOwnMenubar/obj/Debug/net8.0/SetupOwnMenubar.AssemblyInfo.cs
+++ BlazorProjects/SetupOwnMenubar/obj/Debug/net8.0/SetupOwnMenubar.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SetupOwnMenubar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+616d5e9ceb637dc8985fc3257cf696ecb7e8a2e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39fc559127011dcbc7751e824e7c59e3d5c2cdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SetupOwnMenubar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SetupOwnMenubar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
