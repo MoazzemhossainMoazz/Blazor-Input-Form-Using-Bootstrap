916487a48240235834894c3d3571b01ba14dfc53
unknown <moazzemh222@gmail.com>
2025-03-11 23:37:15 +0600

    Added overflow

--- BlazorProjects/BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorProjects/BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d38d66a52731c87d759418c3ec596f173072d66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44dbaa811e0cb6919f03b537355cb0b7ac557ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
